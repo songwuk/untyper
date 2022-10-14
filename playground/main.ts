15bb628982bc71142d8cebf9efd9db097367e478
songwuk <1017301325@qq.com>
2022-10-14 14:27:41 +0800

    docs: update

--- playground/main.ts
+++ playground/main.ts
@@ -1,14 +1,20 @@
 import { UnTyper } from '../src/index'
+import './main.css'
 const text = document.querySelector('#text')! as HTMLElement
-const unTyper = new UnTyper(text, { speed: 100, startDelay: 1000 })
+const unTyper = new UnTyper(text, {
+  speed: 100,
+  startDelay: 1000,
+  // animate: {},
+})
 
-unTyper.type('a simple typewriter for browser', { delay: 200 })
-  .delete(22, { delay: 500 })
-  .type(' typewritr for rowser', { delay: 1200 })
-  .delete(6, { delay: 500 })
-  .pause(1000)
-  .move(-6, { delay: 500 })
-  .type('e', { delay: 1400 })
-  .move(null, { delay: 500, to: 'end' })
-  .type(' browser', { delay: 1200 })
+unTyper
+  .add('a simple ')
+  .add('<span>typewriter</span>')
+  .add(' <em><strong class="font-semibold">for browser</strong></em> and ')
+  .delete(5)
+  .add(' <span>and</span> more support')
+  // .add('<em><strong class="font-semibold">for browser</strong></em>123')
+  // .move(null, { to: 'start', delay: 1000 })
+  // .move(null, { to: 'end' })
+  // .delete(30)
   .go()

e0ea1c4e7861772b45a6c6b2a6cf43d45ea11c03
songwuk <1017301325@qq.com>
2022-10-15 18:21:22 +0800

    docs:  update gif and doc

--- playground/main.ts
+++ playground/main.ts
@@ -6,15 +6,29 @@ const unTyper = new UnTyper(text, {
   startDelay: 1000,
   // animate: {},
 })
+// const lenObject = {
+//   'zh-CN': {
+//     text: '嗨, 我正在成为独立<span>开发者 !</span>',
+//   },
+//   'en-US': {
+//     text: 'Hi, I&apos;m becoming indie <span>developer !</span>',
+//   },
+// }
+// let k = 0
+// for (const i in lenObject) {
+//   k++
+//   if (k === Object.keys(lenObject).length)
+//     unTyper.add(lenObject[i].text).delete(1)
+//   else
+//     unTyper.add(lenObject[i].text).delete(32)
+// }
 
+// unTyper.go()
 unTyper
   .add('a simple ')
   .add('<span>typewriter</span>')
   .add(' <em><strong class="font-semibold">for browser</strong></em> and ')
   .delete(5)
   .add(' <span>and</span> more support')
-  // .add('<em><strong class="font-semibold">for browser</strong></em>123')
-  // .move(null, { to: 'start', delay: 1000 })
-  // .move(null, { to: 'end' })
   // .delete(30)
   .go()

c91c60405dbdf605932798b1285ce0fac142ff8a
songwuk <1017301325@qq.com>
2023-03-07 22:15:50 +0800

    chore: add test file

---
+++ test/untyper.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from 'vitest'
+
+// The two tests marked with concurrent will be run in parallel
+describe('suite', () => {
+  it('serial test', async () => { /* ... */ })
+})
